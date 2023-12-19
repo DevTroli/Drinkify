7c358df23d620e134c331c1e60760df6cafc8ccd
Troli <brawlixo123@gmail.com>
2023-12-19 09:12:04 -0300

    fix: UX error of card

--- src/components/Local/index.tsx
+++ src/components/Local/index.tsx
@@ -22,30 +22,32 @@ const Bares = ({
   destacado,
   descricao
 }: Props) => (
-  <S.Card>
-    <img src={capa} alt="" />
-    <S.Tags>
-      {destacado && <S.Tag>Recomendados</S.Tag>}
-      <S.Tag>{tipo}</S.Tag>
-    </S.Tags>
-    <S.Content>
-      <S.Infos className="sobre">
-        <h3>{titulo}</h3>
-        <div className="center">
-          <h3>{avaliacao}</h3>
-          <img src="/nota.svg" alt="notas" />
-        </div>
-      </S.Infos>
+  <Link to={`Bares/${id}`}>
+    <S.Card>
+      <img src={capa} alt={titulo} />
+      <S.Tags>
+        {destacado && <S.Tag>Recomendados</S.Tag>}
+        <S.Tag>{tipo}</S.Tag>
+      </S.Tags>
+      <S.Content>
+        <S.Infos className="sobre">
+          <h3>{titulo}</h3>
+          <div className="center">
+            <h3>{avaliacao}</h3>
+            <img src="/nota.svg" alt="notas" />
+          </div>
+        </S.Infos>
 
-      <p>{descricao}</p>
+        <p>{descricao}</p>
 
-      <Link to={`Bares/${id}`}>
-        <S.CTA as="a" href="">
-          Saiba mais
-        </S.CTA>
-      </Link>
-    </S.Content>
-  </S.Card>
+        <Link to={`Bares/${id}`}>
+          <S.CTA as="a" href="">
+            Saiba mais
+          </S.CTA>
+        </Link>
+      </S.Content>
+    </S.Card>
+  </Link>
 )
 
 export default Bares
